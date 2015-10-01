1ceb89551e7014f54eb53e2d2b772c1dafd0d21d
Marty Lamb <mlamb@martiansoftware.com>
2015-10-01 09:03:27 -0400

    improved client customization.  accepts optional parameter specifying site location in case any inbound proxies are munging the url

--- src/main/java/com/martiansoftware/martifacts/web/ClientGetter.java
+++ src/main/java/com/martiansoftware/martifacts/web/ClientGetter.java
@@ -1,13 +1,11 @@
 package com.martiansoftware.martifacts.web;
 
 import com.martiansoftware.boom.BoomResponse;
+import static com.martiansoftware.boom.Boom.q;
 import static com.martiansoftware.boom.Boom.request;
 import static com.martiansoftware.boom.Boom.response;
 import com.martiansoftware.boom.MimeType;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.LineNumberReader;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -21,9 +19,9 @@ public class ClientGetter {
     
     // returns a command line client with the URL properly set for this
     // server instance
-    public Object getClient() throws IOException, URISyntaxException {
-        
-        String URLdef = String.format("URL=\"%s\"\n", request().url().replace("/client", ""));
+    public Object getClient() throws IOException, URISyntaxException {        
+        String site = q("site", request().url()).replaceAll("[?#].*", "").replaceAll("/(index\\.html|client)$", "").replaceAll("/+$", "");        
+        String URLdef = String.format("URL=\"%s\"\n", site);
         Path p = Paths.get(ClientGetter.class.getResource("/client/martifacts").toURI());
         
         StringBuilder s = new StringBuilder();
